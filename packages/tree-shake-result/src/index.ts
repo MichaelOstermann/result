b3d93e95c50f265a896cd93e5b27761e0b2777f6
Michael Ostermann <michaelostermann@me.com>
2025-11-09 10:24:16 +0100

    fix: ResultAsync tree-shaking

--- packages/tree-shake-result/src/index.ts
+++ packages/tree-shake-result/src/index.ts
@@ -1,8 +1,10 @@
 import type { TreeShakeImportResolver } from "@monstermann/tree-shake-import-namespaces"
 
 const treeshake: TreeShakeImportResolver = function ({ importAlias, importName, importPath, propertyName }) {
-    if (importPath === "@monstermann/result" && (importName === "Result" || importName === "ResultAsync"))
+    if (importPath === "@monstermann/result" && importName === "Result")
         return `import { ${propertyName} as ${importAlias} } from "@monstermann/result/Result/${propertyName}.js"`
+    if (importPath === "@monstermann/result" && importName === "ResultAsync")
+        return `import { ${propertyName} as ${importAlias} } from "@monstermann/result/ResultAsync/${propertyName}.js"`
     return undefined
 }
 
